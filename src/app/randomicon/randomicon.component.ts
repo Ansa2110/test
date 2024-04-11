2d7b95c4fbe9678b40b85daba37884bda20c3922
Ansa2002 <ansar.ertaev@mail.ru>
2024-04-11 13:46:28 +0600

    corrected

--- src/app/randomicon/randomicon.component.ts
+++ src/app/randomicon/randomicon.component.ts
@@ -2,22 +2,25 @@ import { Component } from '@angular/core';
 import { faAlignJustify, faArrowDownShortWide, faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faAirbnb, faAmazon } from '@fortawesome/free-brands-svg-icons';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-randomicon',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './randomicon.component.html',
   styleUrl: './randomicon.component.scss'
 })
 export class RandomiconComponent {
   icons = [faCoffee, faAlignJustify, faAirbnb, faAmazon, faArrowDownShortWide]
   randomIndex: number | undefined;
-  icon: any;
+  icon: any = undefined;
+  timeout: any;
   getRandomIcon()
   {
-    setTimeout(() => {
-    this.randomIndex = Math.floor(Math.random() * this.icons.length);
-    this.icon = this.icons[this.randomIndex];
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      this.randomIndex = Math.floor(Math.random() * this.icons.length);
+      this.icon = this.icons[this.randomIndex];
     }, 3000)
   }
 }
